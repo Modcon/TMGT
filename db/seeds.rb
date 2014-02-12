128a1f8a6254a0b31ea68f51451886984f453f4b
Modcon <david@modestcon.com>
2014-02-11 16:14:01 -0800

    google font stylings added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	items = Item.create([{name: 'Apple Sauce 8oz', category: 'Jars', price: 5 }, {name: 'Carrot Top Pesto 8oz', category: 'Jars', price: 12 }, {name: 'Tomato Jam 8oz', category: 'Jars', price: 12 }, 
+	{name: 'Pickled Dilly Beans 16oz', category: 'Jars', price: 12 }, {name: 'Preserved Kumquats 12oz', category: 'Jars', price: 15 }, {name: 'Lacto-Fermented Celery Quart', category: 'Jars', price: 15 },
+	{name: 'Lacto-Fermented Cucumbers Quart', category: 'Jars', price: 15 }, {name: 'Preserved Lemons Quart', category: 'Jars', price: 15 }, {name: 'Pickled Blenheim Apricots 8oz', category: 'Jars', price: 12 }, 
+	{name: 'Rhubarb-Ginger Jam 6oz', category: 'Jars', price: 12 }, {name: 'Lemon Vinaigrette 12oz', category: 'Jars', price: 12 }, {name: 'Carmelized Apple Sauce 8oz', category: 'Jars', price: 5 },
+	{name: 'Fat Uncle Farms Roasted Almond Butter 8oz', category: 'Jars', price: 12 }])
