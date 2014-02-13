c9c669319047a1397d6286f022ca68ccaf816e36
Modcon <david@modestcon.com>
2014-02-13 13:15:30 -0800

    CSS, Boxes styled, order button still UGH

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,16 @@
 	{name: 'Pickled Dilly Beans 16oz', category: 'Jars', price: 12 }, {name: 'Preserved Kumquats 12oz', category: 'Jars', price: 15 }, {name: 'Lacto-Fermented Celery Quart', category: 'Jars', price: 15 },
 	{name: 'Lacto-Fermented Cucumbers Quart', category: 'Jars', price: 15 }, {name: 'Preserved Lemons Quart', category: 'Jars', price: 15 }, {name: 'Pickled Blenheim Apricots 8oz', category: 'Jars', price: 12 }, 
 	{name: 'Rhubarb-Ginger Jam 6oz', category: 'Jars', price: 12 }, {name: 'Lemon Vinaigrette 12oz', category: 'Jars', price: 12 }, {name: 'Carmelized Apple Sauce 8oz', category: 'Jars', price: 5 },
-	{name: 'Fat Uncle Farms Roasted Almond Butter 8oz', category: 'Jars', price: 12 }])
+	{name: 'Fat Uncle Farms Roasted Almond Butter 8oz', category: 'Jars', price: 12 }, {name: 'Classic Sandwich Loaf', category: 'Bread', price: 8 }, {name: 'Whole Wheat Sandwich Loaf', category: 'Bread', price: 8 }, 
+	{name: 'Olive Rosemary Loaf', category: 'Bread', price: 10 }, {name: 'Rye Sandwich Loaf', category: 'Bread', price: 7 }, {name: 'Carmelized Apple Loaf', category: 'Bread', price: 8 }, {name: 'Roasted Garlic Boule', category: 'Bread', price: 10 },
+	{name: 'Cinnamon Raisin Loaf', category: 'Bread', price: 8 }, {name: 'Black Sandwich Loaf', category: 'Bread', price: 8 }, 
+	{name: '1lb. Seeded Granola', category: 'Baked Goods', price: 8 }, {name: 'Boxed of Cracked Cookies', category: 'Baked Goods', price: 10 }, {name: '6 Poppy or Sesame Seed Burrito', category: 'Baked Goods', price: 15 },
+	{name: 'Box of Chocolate Brownie Cookies', category: 'Baked Goods', price: 10 }, {name: '2 Preserved Lemon Bars', category: 'Baked Goods', price: 10 }, {name: 'Ground Beef- Rancho San Julian Farms', category: 'Meat', price: 9 },
+	{name: 'Whole Chicken- Lilys Eggs', category: 'Meat', price: 20 }, {name: 'Sweet Italian Sausage- Peads & Barnett', category: 'Meat', price: 11 }, {name: 'Beef Stew Meat- Novy Ranch', category: 'Meat', price: 12 },
+	{name: 'Korean Short Ribs', category: 'Meat', price: 12 }, {name: 'New York Steak', category: 'Meat', price: 24 }, {name: 'Chateaubriand', category: 'Meat', price: 20 }, {name: 'Pork Chops- Peads & Barnett', category: 'Meat', price: 14 },
+	{name: '1lb. Grey Sea Salt', category: 'Bulk', price: 11 }, {name: '5lb. Raw Evaporated Cane Sugar', category: 'Bulk', price: 10 }, {name: '5lb. Bread Flour', category: 'Bulk', price: 8 },
+	{name: '5lb. Rye Flour', category: 'Bulk', price: 8 }, {name: '5lb. Whole Wheat Flour', category: 'Bulk', price: 8 }, {name: '1 Gal. Raw Milk', category: 'Dairy', price: 15 }, {name: 'Half Gal. Raw Milk', category: 'Dairy', price: 10 },
+	{name: '1 Qt. House Yogurt', category: 'Dairy', price: 20 }, {name: '6 Eggs', category: 'Dairy', price: 3.50 }, {name: '12 Eggs', category: 'Dairy', price: 7 },
+	{name: '6 Eggs', category: 'Dairy', price: 3.50 }, {name: '500g Raw Cultured Butter', category: 'Dairy', price: 12 }, {name: '1/4 Gal. Raw Kefir', category: 'Dairy', price: 6 },
+	{name: 'Raw Goats Milk Laundry Soap', category: 'Hall Closet', price: 8 }, {name: 'Raw Goats Milk Body Soap', category: 'Hall Closet', price: 12 },       ])
+	{name: 'Raw Goats Milk Laundry Soap', category: 'Hall Closet', price: 8 }, {name: 'Raw Goats Milk Body Soap', category: 'Hall Closet', price: 12 },       ])
